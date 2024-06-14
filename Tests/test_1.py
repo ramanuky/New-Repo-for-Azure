678fb02e50b4d6efe0dd0d1dd0c03d24d1bb928d
Raya <100232444+ramanuky@users.noreply.github.com>
2024-06-14 17:01:27 +0400

    Add files via upload

---
+++ Tests/test_1.py
@@ -0,0 +1,28 @@
+from Pages.search import SearchPage
+import config
+import testdata
+import logging
+import time
+
+def test(driver):
+
+    si = SearchPage(driver)
+    si.geturl(config.base_url)
+    brand = si.searchInput(testdata.car_name)
+    print(f"brand is - {brand}")
+    print(f"testdata.carname is - {testdata.car_name}")
+    assert brand.lower() == (testdata.car_name).lower(), logging.error("Search is failed")
+    time.sleep(5)   
+    result = si.searchresult()
+    print(f"Result is - {result}")
+    print(f" Result length is - {len(result)}")
+    assert len(result) > 0, logging.error("No result")
+    logging.info("Test passed")
+
+
+# Ray jan generel concept is correct but there is some notes and the MOST IMPORTNAT
+#  NEVER PUT ASSERT In try except block. In this case your test always will pass and never fail even assertion will fail
+
+# Input field locator should be   search_input_loc = (By.XPATH, "//input[@id='searchInp']")
+# As result is return list of elements you should assert len(result)
+# After input car model name a bit time needed, otherwise system can't click enter and ddl of cars not disappeared
